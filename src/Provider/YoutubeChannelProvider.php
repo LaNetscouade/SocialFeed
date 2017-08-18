6295e1bc9907863884267afad9de4b154b9e8338
whiz11 <ab@esokia-webagency.com>
2017-08-18 10:42:50 +0400

    correction youtube channel

---
+++ src/Provider/YoutubeChannelProvider.php
@@ -0,0 +1,101 @@
+<?php
+
+/*
+ * This file is part of the Social Feed Util.
+ *
+ * (c) LaNetscouade <contact@lanetscouade.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Lns\SocialFeed\Provider;
+
+use Lns\SocialFeed\Client\ClientInterface;
+use Lns\SocialFeed\Factory\PostFactoryInterface;
+use Lns\SocialFeed\Model\Feed;
+use Lns\SocialFeed\Model\ResultSet;
+use Lns\SocialFeed\Model\Pagination\Token;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+/**
+ * YoutubePlaylistProvider.
+ */
+class YoutubeChannelProvider extends AbstractProvider
+{
+    private $youtubeApiClient;
+
+    /**
+     * __construct.
+     *
+     * @param ClientInterface      $youtubeApiClient
+     * @param PostFactoryInterface $postFactory
+     */
+    public function __construct(ClientInterface $youtubeApiClient, PostFactoryInterface $postFactory)
+    {
+        $this->youtubeApiClient = $youtubeApiClient;
+        $this->postFactory = $postFactory;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function get(array $parameters = array())
+    {
+        $parameters = $this->resolveParameters($parameters);
+
+
+        $response = $this->youtubeApiClient
+            ->get('/search', array(
+                'query' => array(
+                    'order' => $parameters['order'],
+                    'part' => 'snippet',
+                    'channelId' => $parameters['channelId']
+                ),
+            ));
+
+        $feed = new Feed();
+
+        foreach ($response['items'] as $item) {
+            $feed->addPost($this->postFactory->create($item));
+        }
+
+        return new ResultSet(
+            $feed,
+            $parameters,
+            $this->getNextPaginationToken($response)
+        );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getName()
+    {
+        return 'youtube_channel';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function configureOptionResolver(OptionsResolver &$resolver)
+    {
+        $resolver->setRequired('channelId');
+        $resolver->setDefaults(array(
+            'pageToken' => null,
+        ));
+    }
+
+    protected function getNextPaginationToken($response)
+    {
+        if (!isset($response['nextPageToken'])) {
+            return;
+        }
+
+        return new Token(array(
+            'pageToken' => $response['nextPageToken'],
+        ));
+    }
+
+}
+
