3d0b463acb0fe1a7d7131de6284cdbf64056dc93
whiz11 <ab@esokia-webagency.com>
2017-11-28 15:19:17 +0400

    added new parameter for youtube

--- src/Provider/YoutubeChannelProvider.php
+++ src/Provider/YoutubeChannelProvider.php
@@ -21,8 +21,8 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 /**
  * YoutubeChannelProvider.
  */
-class YoutubeChannelProvider extends AbstractProvider
-{
+class YoutubeChannelProvider extends AbstractProvider {
+
     private $youtubeApiClient;
 
     /**
@@ -31,8 +31,7 @@ class YoutubeChannelProvider extends AbstractProvider
      * @param ClientInterface      $youtubeApiClient
      * @param PostFactoryInterface $postFactory
      */
-    public function __construct(ClientInterface $youtubeApiClient, PostFactoryInterface $postFactory)
-    {
+    public function __construct(ClientInterface $youtubeApiClient, PostFactoryInterface $postFactory) {
         $this->youtubeApiClient = $youtubeApiClient;
         $this->postFactory = $postFactory;
     }
@@ -40,57 +39,78 @@ class YoutubeChannelProvider extends AbstractProvider
     /**
      * {@inheritdoc}
      */
-    public function get(array $parameters = array())
-    {
+    public function get(array $parameters = array()) {
+        
+        $fixed_video = array();
         $parameters = $this->resolveParameters($parameters);
 
+        //get videos of channel
+        $channel_videos = $this->youtubeApiClient
+                ->get('/search', array(
+            'query' => array(
+                'order' => $parameters['order'],
+                'maxResults' => $parameters['maxResults'],
+                'part' => 'snippet',
+                'channelId' => $parameters['channelId']
+            ),
+        ));
 
-        $response = $this->youtubeApiClient
-            ->get('/search', array(
+        //retrieve youtube videoID description
+        if (!empty($parameters['videoID'])) {
+            $video = $this->youtubeApiClient
+                    ->get('/videos', array(
                 'query' => array(
-                    'order' => $parameters['order'],
-                    'maxResults' => $parameters['maxResults'],
                     'part' => 'snippet',
-                    'channelId' => $parameters['channelId']
+                    'id' => $parameters['videoID'],
                 ),
             ));
+            $fixed_video['items'][0]['id']['videoID'] = $video['items'][0]['id'];
+            $fixed_video['kind'] = 'youtube#searchListResponse';
+            $fixed_video['etag'] = $video['etag'];
+            $fixed_video['pageInfo']['totalResults'] = 1;
+            $fixed_video['pageInfo']['resultsPerPage'] = 1;
+            $fixed_video['nextPageToken'] = $channel_videos['nextPageToken'];
+            foreach ($video['items'] as $val) {
+                $fixed_video['items'][0]['kind'] = 'youtube#searchResult';
+                $fixed_video['items'][0]['etag'] = $val['etag'];
+                $fixed_video['items'][0]['id']['kind'] = 'youtube#video';
+                $fixed_video['items'][0]['id']['videoId'] = $val['id'];
+                $fixed_video['items'][0]['snippet'] = $val['snippet'];
+            }
+            $response = array_merge_recursive($fixed_video, $channel_videos);
+        }
 
         $feed = new Feed();
 
         foreach ($response['items'] as $item) {
             $feed->addPost($this->postFactory->create($item));
         }
-
         return new ResultSet(
-            $feed,
-            $parameters,
-            $this->getNextPaginationToken($response)
+                $feed, $parameters, $this->getNextPaginationToken($response)
         );
     }
 
     /**
      * {@inheritdoc}
      */
-    public function getName()
-    {
+    public function getName() {
         return 'youtube_channel';
     }
 
     /**
      * {@inheritdoc}
      */
-    protected function configureOptionResolver(OptionsResolver &$resolver)
-    {
+    protected function configureOptionResolver(OptionsResolver &$resolver) {
         $resolver->setRequired('channelId');
         $resolver->setRequired('order');
         $resolver->setRequired('maxResults');
+        $resolver->setRequired('videoID');
         $resolver->setDefaults(array(
             'pageToken' => null,
         ));
     }
 
-    protected function getNextPaginationToken($response)
-    {
+    protected function getNextPaginationToken($response) {
         if (!isset($response['nextPageToken'])) {
             return;
         }
@@ -101,4 +121,3 @@ class YoutubeChannelProvider extends AbstractProvider
     }
 
 }
-
